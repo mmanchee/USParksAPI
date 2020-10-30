62684990ba72c8737b8e942d4b5d8d66266d81db
Mike Manchee <mikemanchee@gmail.com>
2020-10-30 12:05:03 -0700

    add setup for SeedData

--- USParksAPI/Models/USParksAPIContext.cs
+++ USParksAPI/Models/USParksAPIContext.cs
@@ -2,13 +2,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace USParksAPI.Models
 {
-    public class USParksAPIContext : DbContext
+  public class USParksAPIContext : DbContext
+  {
+    public USParksAPIContext(DbContextOptions<USParksAPIContext> options)
+        : base(options)
     {
-        public USParksAPIContext(DbContextOptions<USParksAPIContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Park> Parks { get; set; }
     }
+
+    public DbSet<Park> Parks { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+          .HasData(
+        new Park { ParkId = 1, State = "", City = "", Description = "" }
+        
+        );
+    }
+  }
 }
