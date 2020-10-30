dce0688f5365bb7a0e5596c00e1346e055ea2f46
Mike Manchee <mikemanchee@gmail.com>
2020-10-30 11:57:27 -0700

    edit Program and Startup for Authentication and Swagger

--- USParksAPI/Program.cs
+++ USParksAPI/Program.cs
@@ -19,6 +19,7 @@ namespace USParksAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:6000"); //Added to enable Token
     }
 }
